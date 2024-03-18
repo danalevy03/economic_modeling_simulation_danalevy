edea4fba443617641f143243d79756bf47ff1056
danalevy <danalevyschettini@gmail.com>
2024-03-18 16:52:03 +0100

    Commit message

--- advanced_point.py
+++ advanced_point.py
@@ -1,3 +1,29 @@
-from inheritance-example import ColorPoint
+from colorpoint import ColorPoint
 
-class AdvancedColorPpint(ColorPoint)
+class AdvancedColorPpint(ColorPoint):
+    def __init__(self, x, y, color):
+        self._x = x
+        self._y = y
+        self._color = color
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, new_color):
+        self._color = color
+
+
+new_point = AdvancedColorPpint(10, 10, "blue")
+print(new_point)
+new_point.color = "red"
+print(new_point)
